349a271b5489524104c42ff0815c65d77065b84b
The Thanh <thethanhnguyen.662@gmail.com>
2021-03-06 15:26:48 +0700

    add admin function :truck:

---
+++ PostSys/Controllers/AdminsController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using PostSys.Models;
+using PostSys.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PostSys.Controllers
+{
+    public class AdminsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public AdminsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Admins
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            //Láy giá trị từ bẳng AspNetUser và liên kết với bảng AspnetRole thông qua bảng AspNetUserRole
+            var UserInfor = (from user in _context.Users
+                             select new
+                             /*FROM-IN: xác định nguồn dữ liệu truy vấn (Users). 
+                             Nguồn dữ liệu tập hợp những phần tử thuộc kiểu lớp triển khai giao diện IEnumrable*/
+                             /*SELECT: chỉ ra các dữ liệu được xuất ra từ nguồn */
+                             {
+                                 UserId = user.Id,
+                                 Username = user.UserName,
+                                 Emailaddress = user.Email,
+                                 RoleName = (from userRole in user.Roles
+                                                 //JOIN kết hợp 2 trường dữ liệu tương ứng
+                                             join role in _context.Roles //JOIN-IN: chỉ ra nguồn kết nối vs nguồn của FROM   
+                                             on userRole.RoleId          //ON: chỉ ra sự ràng buộc giữa các phần tử  
+                                             equals role.Id              //EQUALS: chỉ ra căn cứ vs ràng buộc (userRole.RoleId ~~ role.Id)
+                                             select role.Name).ToList()
+                             }
+                             ).ToList();
+
+
+
+            // gắn giá trị của user with roleName vào UserInRolesViewModel()
+            var UserWithRole = UserInfor.Select(p => new UserInRolesViewModel()
+            {
+                UserId = p.UserId,
+                Username = p.Username,
+                Email = p.Emailaddress,
+                Role = string.Join(",", p.RoleName)
+            }
+                                                );
+
+            return View(UserWithRole);
+        }
+
+
+        //Delete Account
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(string id)
+        {
+            var AccountInDB = _context.Users.SingleOrDefault(p => p.Id == id);
+
+            if (AccountInDB == null)
+            {
+                return HttpNotFound();
+            }
+
+            _context.Users.Remove(AccountInDB);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Edit 
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(string id)
+        {
+            var AccountInDB = _context.Users.SingleOrDefault(p => p.Id == id);
+            if (AccountInDB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(AccountInDB);
+        }
+
+        //EDIT
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(ApplicationUser user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var UsernameIsExist = _context.Users.
+                                  Any(p => p.UserName.Contains(user.UserName));
+
+            if (UsernameIsExist)
+            {
+                ModelState.AddModelError("UserName", "Username already existed");
+                return View();
+            }
+
+            var AccountInDB = _context.Users.SingleOrDefault(p => p.Id == user.Id);
+
+            if (AccountInDB == null)
+            {
+                return HttpNotFound();
+            }
+
+            AccountInDB.UserName = user.UserName;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Reset password
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public ActionResult ChangePass(string id)
+        {
+            var AccountInDB = _context.Users.SingleOrDefault(p => p.Id == id);
+
+            if (AccountInDB == null)
+            {
+                return HttpNotFound();
+            }
+
+            //Khai báo biến var userId thuộc Curent.User.Identity và truy cập vào trường Id thông qua GetUserId       
+            var userId = System.Web.HttpContext.Current.User.Identity.GetUserId();
+            userId = AccountInDB.Id;
+            if (userId != null) //Nếu userId tồn tại
+            {
+                //userManager bằng quản lý người dùng mới, mang dữ liệu mới 
+                UserManager<IdentityUser> userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>());
+                //Xoá password hiện tại của userManager 
+                userManager.RemovePassword(userId);
+                //Thay password mới "123456789" cho userManager
+                String newPassword = "123456789";
+                userManager.AddPassword(userId, newPassword);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PostSys/Startup.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using PostSys.Models;
+
+[assembly: OwinStartupAttribute(typeof(PostSys.Startup))]
+namespace PostSys
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+
+        // In this method we will create default User roles and Admin user for login    
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // Create only Admin Account
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool    
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                   
+
+                var user = new ApplicationUser();
+                user.UserName = "admin";
+                user.Email = "admin@gmail.com";
+
+                string userPWD = "Abc@123";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin    
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+            
+            //Create only Marketing Manager
+            if (!roleManager.RoleExists("Marketing Manager"))
+            {
+
+                // first we create Marketing manager rool   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Marketing Manager";
+                roleManager.Create(role);
+
+                //Here we create a Marketing manager            
+
+                var user = new ApplicationUser();
+                user.UserName = "marketingManager";
+                user.Email = "marketingManager@gmail.com";
+
+                string userPWD = "Abc@123";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin    
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Marketing Manager");
+
+                }
+            }
+
+
+            // creating Creating Marketing Coodinator role     
+            if (!roleManager.RoleExists("Marketing Coodinator"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Marketing Coodinator";
+                roleManager.Create(role);
+
+            }
+
+            // creating Creating Student role     
+            if (!roleManager.RoleExists("Student"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Student";
+                roleManager.Create(role);
+
+            }
+
+            // creating Creating Guest role     
+            if (!roleManager.RoleExists("Guest"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Guest";
+                roleManager.Create(role);
+
+            }
+        }
+    }
+}
